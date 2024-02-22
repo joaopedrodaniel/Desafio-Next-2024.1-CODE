4447621dd0732dde877f567b9bb4e6a744ac5695
João Pedro Daniel <joaopedro.daniel@estudante.ufjf.br>
2024-02-22 01:03:39 -0300

    footer

---
+++ components/footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return(
+        <div className="bg-primarycolor min-h-[150px] text-center flex flex-col justify-center">
+            <span className="text-xl font-bold uppercase">Guitar Tone</span>
+            <span>contato@guitartone.com.br</span>
+            <span>(31) 4002 - 8922</span>
+            <span>@guitartone</span>
+        </div>
+    )
+}
