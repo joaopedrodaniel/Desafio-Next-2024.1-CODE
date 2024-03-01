7af2250de301ab2fcf2046900368a588ab3f4a84
João Pedro Daniel <joaopedro.daniel@estudante.ufjf.br>
2024-03-01 04:03:54 -0300

    crud

--- actions/members/index.tsx
+++ actions/members/index.tsx
@@ -1,8 +1,10 @@
 "use server"
 
 import prisma from "@/lib/db"
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-export default async function getMembers() {
+export async function getMembers() {
     const members = await prisma.membro.findMany({
         where: {
             published: true
@@ -16,4 +18,71 @@ export default async function getMembers() {
     })
 
     return members;
+}
+
+export async function getMemberById(id: number | undefined) {
+    const member = await prisma.membro.findUnique({
+        where: {id},
+        select: {
+            id: true,
+            name: true,
+            cargo: true,
+            email: true
+        },
+    });
+
+    return member;
+}
+
+export async function updateMember(id: number | undefined, formData: FormData) {
+    const name = formData.get("name") as string;
+    const cargo = formData.get("cargo") as string;
+    const email = formData.get("email") as string;
+
+    await prisma.membro.update({
+        where: { id },
+        data: {
+            name,
+            cargo,
+            email,
+        },
+    });
+
+    redirect("/admin/management")
+}
+
+export async function deleteMember(id:number | undefined) {
+    await prisma.membro.delete({
+        where: { id },
+    });
+
+    revalidatePath("/admin/management")
+}
+
+export async function createMember(formData: FormData) {
+    const name = formData.get("name") as string;
+    const cargo = formData.get("cargo") as string;
+    const email = formData.get("email") as string;
+
+    const memberVerification = await prisma.membro.findFirst({
+        where: {
+            OR: [
+                { name },
+                { email },
+            ],
+        },
+    });
+
+    
+
+    await prisma.membro.create({
+        data: {
+            name,
+            cargo,
+            email,
+            published: true,
+        },
+    });
+
+    redirect("/admin/management")
 }
